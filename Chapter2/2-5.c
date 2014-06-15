3ffc8f0d35f87b38f19f8904afed4e2d378fd5ca
jk <jennykang95@gmail.com>
2014-06-15 21:58:08 +0800

    finsihed 2-5.c

---
+++ Chapter2/2-5.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+void getstring (char s[], int size);
+int any (char s1[], char s2[]);
+
+int main () {
+	char c, d;
+	char s1[100], s2[100];
+	int i, j, k;
+	int size, result;
+	size = 100;
+	printf("Please type in a string: \n");
+	getstring (s1, size);
+
+	printf("Please type in another string: \n");
+	getstring (s2, size);
+
+	printf("First location of a character in the second string on the first string is : \n");
+	result = any(s1, s2);
+	printf("%d\n", result);
+
+	return 0;
+}
+
+void getstring (char s[], int size){
+	int i;
+	char c;
+	i = 0;
+	while ((c = getchar()) != '\n' && i < size){
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
+
+}
+
+int any(char s1[], char s2[]){
+	int counter;
+	int i, j;
+	for (i = 0; s1[i] != '\0'; i++){
+		for (j = 0; s2[j] != '\0'; j++){
+			if (s1[i] == s2[j]){
+				return i;
+			}
+		}
+	}	
+
+	return -1;
+}
+
